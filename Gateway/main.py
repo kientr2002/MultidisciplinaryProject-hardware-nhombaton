444735158fac6a000243ce2dd3b39074157793be
kientr2002 <kien.tranvictory@hcmut.edu.vn>
2023-03-16 13:29:18 +0700

    update file test and mqtt for js

--- Gateway/main.py
+++ Gateway/main.py
@@ -4,12 +4,13 @@ import time
 import serial . tools . list_ports
 import paho.mqtt.client as mqtt
 
+import json
+import requests
+
 from Adafruit_IO import MQTTClient
 
-#Adafruit_io
-AIO_FEED_IDS = ["farm-button-led", "farm-button-pump", "farm-humi", "farm-light", "farm-soil-humi", "farm-temp"]
-AIO_USERNAME = "teambaton"
-AIO_KEY = "aio_Jsbj78KF7pj7sgelX7K8C9M6Ljnc"
+#url web
+
 
 #mqtt ohstem
 MQTT_SERVER = "mqtt.ohstem.vn"
@@ -27,6 +28,16 @@ list_feed = [
 ]
 
 # functional
+def subscribe(client , userdata , mid , granted_qos):
+    print("Subscribe thanh cong ...")
+
+def disconnected(client):
+    print("Ngat ket noi ...")
+    sys.exit (1)
+
+def message(client , feed_id , payload):
+    print("Nhan du lieu: " + payload)
+
 def mqtt_connected(client, userdata, flags, rc):
     for feed in list_feed:
         client.subscribe(link + feed)
@@ -37,9 +48,10 @@ def mqtt_subscribed(client, userdata, mid, granted_qos):
 def mqtt_disconnected(client):
     print("Ngat ket noi ...")
     sys.exit (5)
-
+value = ""
 def on_message(client, userdata, message):
     print(str(message.topic)[16:] + ":" + str(message.payload.decode("utf-8")))
+    value = str(message.payload.decode("utf-8"))
 
 mqttClient = mqtt.Client()
 mqttClient.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
@@ -49,20 +61,6 @@ mqttClient.on_subscribe = mqtt_subscribed
 mqttClient.on_message = on_message
 
 mqttClient.loop_start()
-counter = 0
-
 
 while True:
-    # print("Nhap du lieu ma ban muon thay doi (1 la pump, 2 la led):")
-    # type_input = input()
-    # value = 0
-    # if type_input == 1:
-    #     print("Nhap du lieu pump:")
-    #     value = input()
-    #     mqttClient.publish("nhombaton/feeds/V10",value)
-    # elif type_input == 2:
-    #     print("Nhap du lieu gia tri LED:")
-    #     value = input()
-    #     mqttClient.publish("nhombaton/feeds/V11",value)
-    # print("Da cap nhat du lieu")
     pass
