63fa30d485eebd365c30232dcabe818ec45c4230
kientr2002 <kien.tranvictory@hcmut.edu.vn>
2023-03-29 07:51:24 +0700

    up code gateway by using js

--- Gateway/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-url='https://api.ipify.org/'
-headers=''
-data = {'key1': 'value1', 'key2': 'value2'}
-data = {'key1': 'value1', 'key2': 'value2'}
-r=requests.post(url=url,headers=headers)
-status = r.json
-
-print(status)
